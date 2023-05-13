0a39b8e8ef81ce1bf83b0ea8ff0b926b11244689
Dusan Rajkovic <dusanrajkovic39@gmail.com>
2023-05-13 12:42:41 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,40 @@
 <template>
   
   <div class="title-container">
-    <h1>Dna Information Analytics aa</h1>
+    <h1>Dna Information Analytics</h1>
   </div>
 
   <div class="container">
-    <section class="section-one-container">
-      <analytics></analytics>
-    </section>    
+    <div class="chart-container">
+      <Chart v-show="selectedChart === 0" id="myChartSubscriptions"></Chart>
+      <Chart v-show="selectedChart === 1" id="myChartImpressions"></Chart>
+      <Chart v-show="selectedChart === 2" id="myChartClicks"></Chart>
+      <Chart v-show="selectedChart === 3" id="myChartAvgTime"></Chart>
+    </div>
+    
+    <div class="controller-container">
+      <Controller data-type="subscriptions" @click="showChart(0)"></Controller>
+      <Controller data-type="impressions" @click="showChart(1)"></Controller>
+      <Controller data-type="clicks" @click="showChart(2)"></Controller>
+      <Controller data-type="avgTime" @click="showChart(3)"></Controller>
+    </div>   
   </div>
-
 </template>
 
 <script setup>
 
-  import Analytics from "./components/Analytics.vue"
+  import Chart from './components/Chart.vue'
+  import Controller from './components/Controller.vue';
+
+  import { watch, ref } from 'vue' 
+
+  let selectedChart = ref(0)
+
+  const showChart = (chartIndex) => {
+    selectedChart.value = chartIndex
+  }
+
+  watch(() => selectedChart.value, () => {}, { deep: true });
   
 </script>
 
@@ -24,7 +44,7 @@
     justify-content: center;
     color: white;
     background-color: #146C94;
-    margin-bottom: 5%;
+    margin-bottom: 2%;
 
     h1{
       padding: 20px 40px;
@@ -41,20 +61,61 @@
     flex-direction: column;
     align-items: center;
 
-    .section-one-container{
+    .chart-container{
+      width: 50%;
+      padding: 3% 4%;
+      border: solid 2px #F6F1F1;
+      border-radius: 20px;
+    }
+
+    .controller-container{
       display: flex;
-      justify-content: center;
-      flex-direction: column;
+      flex-direction: row;
     }
   }
 
-  @media (max-width: 850px){
-    .title-container{
+    @media (max-width: 850px){
+      .title-container{
 
-      h1{
-        font-size: 18px;
+        h1{
+          font-size: 18px;
+        }
+      }
+
+      .container{
+        .chart-container{
+          width: 500px !important;
+        }
+
+        .controller-container{
+          display: grid !important;
+          column-gap: 30px;
+          row-gap: 30px;
+          grid-template-columns: 150px 150px;
+        }
       }
     }
-  }
+
+    @media (max-width: 550px){
+      .container{
+        .chart-container{
+          width: 350px !important;
+        }
+
+        .controller-container{
+          column-gap: 5px;
+          row-gap: 5px;
+          grid-template-columns: 140px 140px;
+        }
+      }
+    }
+
+    @media (max-width: 410px){
+      .container{
+        .chart-container{
+          width: 290px !important;
+        }  
+      }
+    }
 
 </style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,173 @@
+<template>
+
+  <canvas id="myChart"></canvas>
+    
+</template>
+
+<script> 
+
+  import { getData } from '../api';
+  import Chart from 'chart.js/auto';
+  
+  var subscriptionsLabels = []
+  var subscriptionsData = []
+  var impressionsLabels = []
+  var impressionsData = []
+  var clicksLabels = []
+  var clicksData = []
+  var avgTimeLabels = []
+  var avgTimeData = []
+  
+  async function getDataAPI(){
+    const response = await getData()
+    return response
+  }
+
+  async function getSubscriptionsData(response){
+    const subscriptions = response.data.subscriptions
+    const subscriptionsKey = Object.keys(subscriptions.history)
+    const subscriptionsValues = Object.values(subscriptions.history)
+
+    subscriptionsLabels = subscriptionsKey
+    subscriptionsData = subscriptionsValues
+
+    await createChartSubscriptions()
+  }
+
+  async function getImpressionsData(response){
+    const impressions = response.data.impressions
+    const impressionsKey = Object.keys(impressions.history)
+    const impressionsValues = Object.values(impressions.history)
+
+    impressionsLabels = impressionsKey
+    impressionsData = impressionsValues
+
+    await createChartImpressions()
+  }
+
+  async function getClicksData(response){
+    const clicks = response.data.clicks
+    const clicksKey = Object.keys(clicks.history)
+    const clicksValues = Object.values(clicks.history)
+
+    clicksLabels = clicksKey
+    clicksData = clicksValues 
+
+    await createChartClicks()
+  }
+
+  async function getAvgTimeData(response){
+    const avgTime = response.data.avgTime
+    const avgTimeKey = Object.keys(avgTime.history)
+    const avgTimeValues = Object.values(avgTime.history)
+    
+    avgTimeLabels = avgTimeKey
+    avgTimeData = avgTimeValues   
+
+    await createChartAvgTime()
+  }
+
+  async function createChartSubscriptions(){
+    const data = {    
+      labels: subscriptionsLabels,
+      datasets: [{
+        label: 'Subscriptions',
+        backgroundColor: '#19A7CE',
+        borderColor: '#19A7CE',
+        data: subscriptionsData,
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChartSubscriptions = new Chart(document.getElementById('myChartSubscriptions'), config);
+  }
+
+  async function createChartImpressions(){
+
+    const data = {    
+      labels: impressionsLabels,
+      datasets: [{
+        label: 'Impressions',
+        backgroundColor: '#19A7CE',
+        borderColor: '#19A7CE',
+        data: impressionsData,
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChartImpressions = new Chart(document.getElementById('myChartImpressions'), config);
+  }
+
+  async function createChartClicks(){
+
+    const data = {    
+      labels: clicksLabels,
+      datasets: [{
+        label: 'Clicks',
+        backgroundColor: '#19A7CE',
+        borderColor: '#19A7CE',
+        data: clicksData,
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChartClicks = new Chart(document.getElementById('myChartClicks'), config);
+  }
+
+  async function createChartAvgTime(){
+
+    const data = {    
+      labels: avgTimeLabels,
+      datasets: [{
+        label: 'Avg Time',
+        backgroundColor: '#19A7CE',
+        borderColor: '#19A7CE',
+        data: avgTimeData,
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {}
+    };
+
+    const myChartavgTime = new Chart(document.getElementById('myChartAvgTime'), config);
+  }
+
+  async function getDataAndCallFunctions() {
+    try{
+      const response = await getDataAPI()
+      getSubscriptionsData(response)
+      getImpressionsData(response)
+      getClicksData(response)
+      getAvgTimeData(response)
+    }catch(error){
+      console.error(error)
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    getDataAndCallFunctions()
+  });
+
+</script>
+
+<style lang="scss">
+
+</style>
