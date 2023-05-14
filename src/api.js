f8f64e895eae21db9658179bb45b1baf8d891245
Dusan Rajkovic <dusanrajkovic39@gmail.com>
2023-05-14 15:39:51 +0200

    refactoring creazione chart

--- src/api.js
+++ src/api.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 
 const VITE_API_CHARTS = import.meta.env.VITE_API_CHARTS
+
+var subscriptionsLabels = []
+var subscriptionsData = []
+var impressionsLabels = []
+var impressionsData = []
+var clicksLabels = []
+var clicksData = []
+var avgTimeLabels = []
+var avgTimeData = []
   
 export async function getData(){
   try{
@@ -14,9 +23,57 @@ export async function getData(){
 async function getDataAndCallFunctions() {
   try{
     const response = await getData()
+    getSubscriptionsData(response)
+    getImpressionsData(response)
+    getClicksData(response)
+    getAvgTimeData(response)
   }catch(error){
     console.error(error)
   }
+}
+
+export async function getSubscriptionsData(response){
+  const subscriptions = response.data.subscriptions
+  const subscriptionsKey = Object.keys(subscriptions.history)
+  const subscriptionsValues = Object.values(subscriptions.history)
+
+  subscriptionsLabels = subscriptionsKey
+  subscriptionsData = subscriptionsValues
+
+  return { subscriptionsLabels, subscriptionsData }
+}
+
+export async function getImpressionsData(response){
+  const impressions = response.data.impressions
+  const impressionsKey = Object.keys(impressions.history)
+  const impressionsValues = Object.values(impressions.history)
+
+  impressionsLabels = impressionsKey
+  impressionsData = impressionsValues
+
+  return { impressionsLabels, impressionsData }
+}
+
+export async function getClicksData(response){
+  const clicks = response.data.clicks
+  const clicksKey = Object.keys(clicks.history)
+  const clicksValues = Object.values(clicks.history)
+
+  clicksLabels = clicksKey
+  clicksData = clicksValues 
+
+  return { clicksLabels, clicksData }
+}
+
+export async function getAvgTimeData(response){
+  const avgTime = response.data.avgTime
+  const avgTimeKey = Object.keys(avgTime.history)
+  const avgTimeValues = Object.values(avgTime.history)
+  
+  avgTimeLabels = avgTimeKey
+  avgTimeData = avgTimeValues
+  
+  return { avgTimeLabels, avgTimeData }
 }
 
 window.addEventListener('DOMContentLoaded', () => {
--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -6,164 +6,66 @@
 
 <script> 
 
-  import { getData } from '../api';
+  import { getData, getSubscriptionsData, getImpressionsData, getClicksData, getAvgTimeData } from '../api';
   import Chart from 'chart.js/auto';
-  
-  var subscriptionsLabels = []
-  var subscriptionsData = []
-  var impressionsLabels = []
-  var impressionsData = []
-  var clicksLabels = []
-  var clicksData = []
-  var avgTimeLabels = []
-  var avgTimeData = []
-  
-  async function getDataAPI(){
+
+  async function createCharts() {
     const response = await getData()
-    return response
+
+    const subscriptionsDataAPI= await getSubscriptionsData(response)
+    const impressionsDataAPI = await getImpressionsData(response)
+    const clicksDataAPI = await getClicksData(response)
+    const avgTimeDataAPI = await getAvgTimeData(response)
+
+    const { subscriptionsLabels, subscriptionsData } = subscriptionsDataAPI
+    const { impressionsLabels, impressionsData } = impressionsDataAPI
+    const { clicksLabels, clicksData } = clicksDataAPI
+    const { avgTimeLabels, avgTimeData } = avgTimeDataAPI
+
+    createChartSubscriptions(subscriptionsLabels, subscriptionsData)
+    createChartImpressions(impressionsLabels, impressionsData)
+    createChartClicks(clicksLabels, clicksData)
+    createChartAvgTime(avgTimeLabels, avgTimeData)
   }
 
-  async function getSubscriptionsData(response){
-    const subscriptions = response.data.subscriptions
-    const subscriptionsKey = Object.keys(subscriptions.history)
-    const subscriptionsValues = Object.values(subscriptions.history)
-
-    subscriptionsLabels = subscriptionsKey
-    subscriptionsData = subscriptionsValues
-
-    await createChartSubscriptions()
-  }
-
-  async function getImpressionsData(response){
-    const impressions = response.data.impressions
-    const impressionsKey = Object.keys(impressions.history)
-    const impressionsValues = Object.values(impressions.history)
-
-    impressionsLabels = impressionsKey
-    impressionsData = impressionsValues
-
-    await createChartImpressions()
-  }
-
-  async function getClicksData(response){
-    const clicks = response.data.clicks
-    const clicksKey = Object.keys(clicks.history)
-    const clicksValues = Object.values(clicks.history)
-
-    clicksLabels = clicksKey
-    clicksData = clicksValues 
-
-    await createChartClicks()
-  }
-
-  async function getAvgTimeData(response){
-    const avgTime = response.data.avgTime
-    const avgTimeKey = Object.keys(avgTime.history)
-    const avgTimeValues = Object.values(avgTime.history)
-    
-    avgTimeLabels = avgTimeKey
-    avgTimeData = avgTimeValues   
-
-    await createChartAvgTime()
-  }
-
-  async function createChartSubscriptions(){
-    const data = {    
-      labels: subscriptionsLabels,
-      datasets: [{
-        label: 'Subscriptions',
-        backgroundColor: '#19A7CE',
-        borderColor: '#19A7CE',
-        data: subscriptionsData,
-      }]
-    };
-
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-
+  async function createChartSubscriptions(labels, data) {
+    const config = createConfig(labels, data, 'Subscriptions');
     const myChartSubscriptions = new Chart(document.getElementById('myChartSubscriptions'), config);
   }
 
-  async function createChartImpressions(){
-
-    const data = {    
-      labels: impressionsLabels,
-      datasets: [{
-        label: 'Impressions',
-        backgroundColor: '#19A7CE',
-        borderColor: '#19A7CE',
-        data: impressionsData,
-      }]
-    };
-
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-
+  async function createChartImpressions(labels, data) {
+    const config = createConfig(labels, data, 'Impressions');
     const myChartImpressions = new Chart(document.getElementById('myChartImpressions'), config);
   }
 
-  async function createChartClicks(){
-
-    const data = {    
-      labels: clicksLabels,
-      datasets: [{
-        label: 'Clicks',
-        backgroundColor: '#19A7CE',
-        borderColor: '#19A7CE',
-        data: clicksData,
-      }]
-    };
-
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-
+  async function createChartClicks(labels, data) {
+    const config = createConfig(labels, data, 'Clicks');
     const myChartClicks = new Chart(document.getElementById('myChartClicks'), config);
   }
 
-  async function createChartAvgTime(){
-
-    const data = {    
-      labels: avgTimeLabels,
-      datasets: [{
-        label: 'Avg Time',
-        backgroundColor: '#19A7CE',
-        borderColor: '#19A7CE',
-        data: avgTimeData,
-      }]
-    };
-
-    const config = {
-      type: 'line',
-      data: data,
-      options: {}
-    };
-
-    const myChartavgTime = new Chart(document.getElementById('myChartAvgTime'), config);
+  async function createChartAvgTime(labels, data) {
+    const config = createConfig(labels, data, 'Avg Time');
+    const myChartAvgTime = new Chart(document.getElementById('myChartAvgTime'), config);
   }
 
-  async function getDataAndCallFunctions() {
-    try{
-      const response = await getDataAPI()
-      getSubscriptionsData(response)
-      getImpressionsData(response)
-      getClicksData(response)
-      getAvgTimeData(response)
-    }catch(error){
-      console.error(error)
-    }
+  function createConfig(labels, data, label) {
+    return {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: label,
+          backgroundColor: '#19A7CE',
+          borderColor: '#19A7CE',
+          data: data,
+        }]
+      },
+      options: {}
+    };
   }
 
   window.addEventListener('DOMContentLoaded', () => {
-    getDataAndCallFunctions()
+    createCharts();
   });
 
 </script>
